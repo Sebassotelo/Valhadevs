c77c9c8f1ca39f2b4e8fcffc26c66cc692a11e27
Sebassotelo <65528710+Sebassotelo@users.noreply.github.com>
2020-05-24 19:58:01 -0300

    Add files via upload

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -10,15 +10,15 @@ $header .= "X-Mailer:PHP/".phpversion()."\r\n";
 $header .= "Mime-Version:1.0 \r\n";
 $header .= "content-Type:text/plain";
 
-$message = "este message fue enviado por: ".$name."\r\n";
+$message = "Este mensaje fue enviado por: ".$nombre."\r\n";
 $message .= "Su e-mail es: ".$mail."\r\n";
-$message .= "Telefono: ".$subject."\r\n";
-$message .= "message: ".$_POST['message']."\r\n";
-$message .= "enviado el: ".date('d/m/Y',time());
+$message .= "Asunto: ".$telefono."\r\n";
+$message .= "Mensaje: ".$_POST['message']."\r\n";
+$message .= "Enviado el: ".date('d/m/Y',time());
 
 
-$destino = 'Valhaweb@gmail.com';
-$asunto = 'mensaje de mi web';
+$para = 'valhadevs@gmail.com';
+$asunto = 'Mensaje de mi web';
 
 if(mail($para,$asunto,utf8_decode($message),$header))
 echo "<script type='text/javascript'>alert('Tu message ha sido enviado exitosamente');</script>";
